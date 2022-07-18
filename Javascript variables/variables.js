2edf944eac41059868e62f35da52dfa47193a0ca
Alan Vilalte <alan9106@gmail.com>
2022-07-18 15:36:06 -0300

    :sparkles: First commit JS Variables

---
+++ Javascript variables/variables.js
@@ -0,0 +1,24 @@
+let name = "Alan";
+let age = "31";
+let isSingle = false;
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const skillers = [
+  "Adrian Fornaso",
+  "Camila Stefano",
+  "Tomás Napolitano",
+  "Matias Spirilis",
+  "Marcos Escandon",
+];
+let allVariables = {
+  name: "Alan",
+  age: "31",
+  isSingle: false,
+  list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  skillers: [
+    "Adrian Fornaso",
+    "Camila Stefano",
+    "Tomás Napolitano",
+    "Matias Spirilis",
+    "Marcos Escandon",
+  ],
+};
