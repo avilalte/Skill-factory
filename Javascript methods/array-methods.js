e35a16f24d8c62bef44084fe9e0521c493fdacfa
Alan Vilalte <alan9106@gmail.com>
2022-07-25 21:00:00 -0300

    :sparkles: Javascript Methods

---
+++ Javascript methods/array-methods.js
@@ -0,0 +1,25 @@
+// Array methods:
+
+// const skillers = ["Adrian", "Williams", "Gonzalo", "Sofía", "Mariano"];
+
+const skillers = ["Gonzalo", "Alan", "Mariano", "German", "Maximiliano"]; // Original array of skillers.
+
+const orderedArray = [...skillers].sort(); // Sorts names in alphabetical order. Returns mutated array.
+const orderedByLength = [...skillers].sort((a, b) => a.length - b.length); // Sorts names by length. Returns mutated array.
+const reversedArray = [...skillers].reverse(); // Reverses array. Returns mutated array.
+const newAtEnd = [...skillers].push("John Doe"); // Adds a name to the end of the array. Returns length of the new array.
+const newAtStart = [...skillers].unshift("John Doe"); // Adds a name to the beginning of the array. Returns length of the new array.
+const firstOut = [...skillers].shift(); // Removes first name from the array. Returns added name.
+const lastOut = [...skillers].pop(); // Removes last name from the array. Returns added name.
+
+console.log(
+  `%c Original array: ${skillers}`,
+  "background: #3693af; color: white"
+); // Original, unmutated array.
+console.log("Ordered by length: ", orderedByLength);
+console.log("Ordered alphabetically: ", orderedArray);
+console.log("Reversed array: ", reversedArray);
+console.log("Name added at the end, new length: ", newAtEnd);
+console.log("Name added at the beginning, new length: ", newAtStart);
+console.log("First name out: ", firstOut);
+console.log("Last name out: ", lastOut);
---
+++ Javascript methods/compare-function.js
@@ -0,0 +1,20 @@
+// Conditional statements:
+
+const compare = () => {
+  const ask = (msg) => parseInt(prompt(msg));
+
+  let input1 = ask("Enter first input");
+  let input2 = ask("Enter second input");
+
+  while (isNaN(input1) || isNaN(input2)) {
+    alert("Inputs must be numbers, try again.");
+    input1 = ask("Enter first input");
+    input2 = ask("Enter second input");
+  }
+
+  if (input1 < input2) return alert("First input is smaller");
+  else if (input1 > input2) return alert("Second input is smaller");
+  else return alert("Inputs are equal");
+};
+
+compare();
