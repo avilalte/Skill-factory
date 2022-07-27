239900a480f185d060688c75fcc73b36f4dbb70c
Alan Vilalte <alan9106@gmail.com>
2022-07-27 03:05:08 -0300

    :hammer: Refactor function

--- Javascript methods/compare-function.js
+++ Javascript methods/compare-function.js
@@ -1,16 +1,18 @@
 // Conditional statements:
 
 const compare = () => {
-  const ask = msg => parseInt(prompt(msg));
+  let input1 = prompt("Enter first input");
+  let input2 = prompt("Enter second input");
+  const checkNum = (input) => (/^-?\d+$/).test(input);
 
-  let input1 = ask("Enter first input");
-  let input2 = ask("Enter second input");
-
-  while (isNaN(input1) || isNaN(input2)) {
+  while (!checkNum(input1) || !checkNum(input2)) {
     alert("Inputs must be numbers, try again.");
-    input1 = ask("Enter first input");
-    input2 = ask("Enter second input");
+    input1 = prompt("Enter first input");
+    input2 = prompt("Enter second input");
   }
+
+  input1 = parseInt(input1);
+  input2 = parseInt(input2);
 
   if (input1 < input2) return alert("First input is smaller");
   else if (input1 > input2) return alert("Second input is smaller");
